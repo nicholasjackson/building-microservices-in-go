a759ae83dda2eb67ee7440304cf4b155f7b31cfa
root <root@consul.acet.io>
2016-07-30 12:04:57 -0400

    updated examples

--- chapter1/basic_http_server.go
+++ chapter1/basic_http_server.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	http.HandleFunc("/helloworld", helloWorldHandler)
 
+	log.Printf("Server starting on port %v\n", 8080)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
-	log.Printf("Server running on port %i\n", 8080)
 }
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
--- chapter1/reading_writing_json_6.go
+++ chapter1/reading_writing_json_6.go
@@ -2,11 +2,47 @@ package main
 
 import (
 	"encoding/json"
+	"golang.org/x/net/context"
 	"log"
 	"net/http"
+	"sync"
 )
 
-var name string
+type key int
+
+const nameKey key = 0
+
+type requestContext struct {
+	context *context.Context
+	cancel  context.CancelFunc
+}
+
+var contexts = map[*http.Request]requestContext{}
+var contextLock sync.Mutex
+var ctx = context.Background()
+
+// returns the Context for our request, creates a new Context if one does not exist
+func contextForRequest(r *http.Request) *context.Context {
+	contextLock.Lock()
+	defer contextLock.Unlock()
+	if c, ok := contexts[r]; ok {
+		return c.context // context has already been created
+	} else {
+		c, cancel := context.WithCancel(ctx) // copy root and add to map
+		contexts[r] = requestContext{context: &c, cancel: cancel}
+		return &c
+	}
+}
+
+// deletes the context for our request, must be called or we will leak memory
+func deleteContextForRequest(r *http.Request) {
+	contextLock.Lock()
+	defer contextLock.Unlock()
+
+	contexts[r].cancel()
+
+	delete(contexts, r)
+}
 
 type helloWorldResponse struct {
 	Message string `json:"message"`
@@ -49,12 +85,19 @@ func (h validationHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(rw, "Bad request", http.StatusBadRequest)
 	} else {
-		name = request.Name
+		ctx := contextForRequest(r)
+		*ctx = context.WithValue(*ctx, nameKey, request.Name)
+
 		h.next.ServeHTTP(rw, r)
 	}
 }
 
 func (h helloWorldHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	ctx := *contextForRequest(r)
+	defer deleteContextForRequest(r) // cleanup
+
+	name := ctx.Value(nameKey).(string)
+
 	response := helloWorldResponse{Message: "Hello " + name}
 
 	encoder := json.NewEncoder(rw)
