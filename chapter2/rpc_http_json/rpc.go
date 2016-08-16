32752369c81db72f74161666d74d8ffc799ef110
Nic Jackson <jackson.nic@gmail.com>
2016-08-16 20:01:14 +0100

    update instruction

--- chapter2/rpc_http_json/rpc.go
+++ chapter2/rpc_http_json/rpc.go
@@ -2,7 +2,8 @@ package main
 
 import "github.com/nicholasjackson/building-microservices-in-go/chapter2/rpc/server"
 
-//curl -X POST -H "Content-Type: application/json" -d '{"id": 1, "method": "HelloWorldHandler.HelloWorld", "params": [{"name":"World"}]}' http://localhost:1234
+// To execute a request with this server run the below command on your command line
+// curl -X POST -H "Content-Type: application/json" -d '{"id": 1, "method": "HelloWorldHandler.HelloWorld", "params": [{"name":"World"}]}' http://localhost:1234
 func main() {
 	server.StartServer()
 }
