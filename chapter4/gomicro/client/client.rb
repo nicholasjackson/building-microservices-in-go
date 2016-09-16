36408daff937d354700e2140706a5222fd18f13b
Nic Jackson <jackson.nic@gmail.com>
2016-09-16 22:09:52 +0100

    Working ruby example using protobufs over http to micro

--- chapter4/gomicro/client/client.rb
+++ chapter4/gomicro/client/client.rb
@@ -6,37 +6,52 @@ $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
 require 'grpc'
 require 'multi_json'
-require 'kittens_services_pb'
+require 'kittens_pb'
 require 'net/http'
 require 'uri'
-require 'google/protobuf'
 
 include Bmigo::Micro
 
-class Kittenserver
-    include Google::Protobuf
-
-    optional :string, :service_method, 1
-    optional :string, :seq, 2
-end
-
 def main()
-    # Create request
-    kittenserver = Kittenserver.new
-    kittenserver.service_method = "something"
-    kittenserver.seq = "1234fddfd" 
-
     puts "Connecting to Kittenserver"
     http = Net::HTTP.new('kittenserver_kittenserver_1',8091)
-    request = Net::HTTP::Post.new('/rpc')
-    request.content_type = 'application/json'
+    request = Net::HTTP::Post.new('/')
+    request.content_type = 'application/octet-stream'
 
+    # Create envelope
+    envelope = RequestEnvelope.new(service_method: "Kittens.List", seq: 2)
+    env = RequestEnvelope.encode(envelope)
+    envSize = Array(env.length).pack('N*') 
+
+    # Create message
     message = Request.new(name: "World")
-    request.body = Kittenserver.encode_json(kittenserver)
-    request.body = request.body + request.encode_json(message)
-    puts request.body
+    mess = Request.encode(message)
+    messSize = Array(mess.length).pack('N*')
+
+    request.body = envSize + env + messSize + mess
     response = http.request(request)
-    puts response.body
+
+    puts response.body.inspect
+    body = response.body
+    
+    sizeByteLength = 4
+    endPos = 3
+   
+    len = body[0..endPos].unpack('N*')[0]
+    
+    endPos = endPos + len
+    resp = body[sizeByteLength..endPos]
+    envelope = ResponseEnvelope.decode(resp)
+   
+    puts envelope.inspect
+    
+    len = body[endPos+1..endPos+sizeByteLength].unpack('N*')[0]
+    
+    endPos = endPos + sizeByteLength
+    resp = body[endPos+1..endPos+len] 
+    message = Response.decode(resp)
+   
+    puts message.inspect
 end
 
 main
