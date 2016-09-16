81d5faa84a53472ebaf67a66ac3515d721369a97
Nic Jackson <jackson.nic@gmail.com>
2016-09-16 12:00:41 -0400

    updated to add protobuf gem to gemfile

--- chapter4/gomicro/client/client.rb
+++ chapter4/gomicro/client/client.rb
@@ -9,17 +9,31 @@ require 'multi_json'
 require 'kittens_services_pb'
 require 'net/http'
 require 'uri'
+require 'google/protobuf'
 
 include Bmigo::Micro
 
+class Kittenserver
+    include Google::Protobuf
+
+    optional :string, :service_method, 1
+    optional :string, :seq, 2
+end
+
 def main()
+    # Create request
+    kittenserver = Kittenserver.new
+    kittenserver.service_method = "something"
+    kittenserver.seq = "1234fddfd" 
+
     puts "Connecting to Kittenserver"
     http = Net::HTTP.new('kittenserver_kittenserver_1',8091)
     request = Net::HTTP::Post.new('/rpc')
     request.content_type = 'application/json'
 
     message = Request.new(name: "World")
-    request.body = Request.encode_json(message)
+    request.body = Kittenserver.encode_json(kittenserver)
+    request.body = request.body + request.encode_json(message)
     puts request.body
     response = http.request(request)
     puts response.body
