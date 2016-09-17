6dd8ee384d625ca6ebcade1ed15de232ca3546a4
Nic Jackson <jackson.nic@gmail.com>
2016-09-17 12:05:44 +0100

    updated example for gomicro

--- chapter4/gomicro/client/client.rb
+++ chapter4/gomicro/client/client.rb
@@ -12,45 +12,69 @@ require 'uri'
 
 include Bmigo::Micro
 
+MESSAGE_SIZE_BYTES = 4
+
+def read_response(body)
+
+  envelope, envelope_size = read_envelope(body) 
+
+  start_pos = MESSAGE_SIZE_BYTES + envelope_size
+  message = read_message(start_pos, body)
+
+  return envelope, message
+end
+
+def read_envelope(body)
+  end_pos = MESSAGE_SIZE_BYTES - 1
+ 
+  message_len = body[0..end_pos].unpack('N*')[0]
+  
+  end_pos = end_pos + message_len
+  resp = body[MESSAGE_SIZE_BYTES..end_pos]
+
+  return ResponseEnvelope.decode(resp), message_len
+end
+
+def read_message(start_pos, body)
+  end_pos = start_pos + MESSAGE_SIZE_BYTES
+  message_len = body[start_pos..end_pos].unpack('N*')[0]
+  start_pos = end_pos
+  end_pos = start_pos + message_len
+  message = body[start_pos..end_pos]
+  puts message.inspect  
+  Response.decode(message)
+end
+
+def send_request()
+
+  http = Net::HTTP.new('kittenserver_kittenserver_1',8091)
+  request = Net::HTTP::Post.new('/')
+  request.content_type = 'application/octet-stream'
+
+  # Create envelope
+  envelope = RequestEnvelope.new(service_method: "Kittens.List", seq: 2)
+  encoded_envelope = RequestEnvelope.encode(envelope)
+  envelope_size = Array(encoded_envelope.length).pack('N*') 
+
+  # Create message
+  message = Request.new(name: "World")
+  encoded_message = Request.encode(message)
+  message_size = Array(encoded_message.length).pack('N*')
+
+  request.body = envelope_size + encoded_envelope + message_size + encoded_message
+  response = http.request(request)
+
+  return response
+end
+
 def main()
+
     puts "Connecting to Kittenserver"
-    http = Net::HTTP.new('kittenserver_kittenserver_1',8091)
-    request = Net::HTTP::Post.new('/')
-    request.content_type = 'application/octet-stream'
 
-    # Create envelope
-    envelope = RequestEnvelope.new(service_method: "Kittens.List", seq: 2)
-    env = RequestEnvelope.encode(envelope)
-    envSize = Array(env.length).pack('N*') 
-
-    # Create message
-    message = Request.new(name: "World")
-    mess = Request.encode(message)
-    messSize = Array(mess.length).pack('N*')
-
-    request.body = envSize + env + messSize + mess
-    response = http.request(request)
-
-    puts response.body.inspect
-    body = response.body
+    body = send_request().body
+    envelope, message = read_response(body)    
     
-    sizeByteLength = 4
-    endPos = 3
-   
-    len = body[0..endPos].unpack('N*')[0]
-    
-    endPos = endPos + len
-    resp = body[sizeByteLength..endPos]
-    envelope = ResponseEnvelope.decode(resp)
-   
-    puts envelope.inspect
-    
-    len = body[endPos+1..endPos+sizeByteLength].unpack('N*')[0]
-    
-    endPos = endPos + sizeByteLength
-    resp = body[endPos+1..endPos+len] 
-    message = Response.decode(resp)
-   
+    puts envelope.inspect 
     puts message.inspect
 end
 
