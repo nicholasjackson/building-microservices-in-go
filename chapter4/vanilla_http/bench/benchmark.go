2b2aac2a1fc05b6020bf4bd90ae3e8c07c01df85
Nic Jackson <jackson.nic@gmail.com>
2016-10-01 15:53:48 +0100

    Added vanilla http example

---
+++ chapter4/vanilla_http/bench/benchmark.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/nicholasjackson/bench"
+	"github.com/nicholasjackson/bench/output"
+	"github.com/nicholasjackson/bench/util"
+	"github.com/nicholasjackson/building-microservices-in-go/chapter4/vanilla_http/entities"
+)
+
+func main() {
+	fmt.Println("Benchmarking application")
+
+	b := bench.New(4000, 180*time.Second, 90*time.Second, 5*time.Second)
+	b.AddOutput(200*time.Second, os.Stdout, output.WriteTabularData)
+	b.AddOutput(1*time.Second, util.NewFile("./output.txt"), output.WriteTabularData)
+	b.AddOutput(1*time.Second, util.NewFile("./error.txt"), output.WriteErrorLogs)
+	b.AddOutput(1*time.Second, util.NewFile("./output.png"), output.PlotData)
+	b.RunBenchmarks(GoMicroRequest)
+}
+
+// GoMicroRequest is executed by benchmarks
+func GoMicroRequest() error {
+
+	request := entities.HelloWorldRequest{
+		Name: "Nic",
+	}
+
+	data, _ := json.Marshal(request)
+
+	req, err := http.NewRequest("POST", "http://consul.acet.io:8080/helloworld", bytes.NewBuffer(data))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.DefaultClient
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Failed with status: %v", resp.Status)
+	}
+
+	return nil
+}
